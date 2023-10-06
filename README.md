57bbbc24178d30fcf728b56bd3573eb2e86f7c89
saltiyazan <yazan.salti.19@gmail.com>
2023-10-06 14:39:03 +0400

    Implements requirer side of sdcore management (#17)

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Charmed Operator for SD-Core's NMS.
 ## Usage
 
 ```bash
-juju deploy sdcore-nms --channel=edge --config webui-endpoint=<webui endpoint>
+juju deploy sdcore-nms --channel=edge
 ```
 
 ## Integrate with ingress
@@ -13,8 +13,12 @@ juju deploy sdcore-nms --channel=edge --config webui-endpoint=<webui endpoint>
 ```bash
 juju deploy traefik-k8s --trust --config external_hostname=<your hostname> --config routing_mode=subdomain
 juju deploy sdcore-upf --channel=edge --trust
+juju deploy mongodb-k8s --trust --channel=5/edge
+juju deploy sdcore-webui --trust --channel=edge
+juju integrate mongodb-k8s sdcore-webui
 juju integrate sdcore-nms:ingress traefik-k8s:ingress
 juju integrate sdcore-nms:fiveg_n4 sdcore-upf:fiveg_n4
+juju integrate sdcore-nms:sdcore-management sdcore-webui:sdcore-management
 ```
 
 You should now be able to access the NMS at `https://<model name>-sdcore-nms.<your hostname>`
--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -2,7 +2,7 @@
 # See LICENSE file for licensing details.
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
 from ops import testing
 from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
@@ -11,6 +11,8 @@ from charm import SDCoreNMSOperatorCharm
 
 FIVEG_N4_RELATION_NAME = "fiveg_n4"
 TEST_FIVEG_N4_PROVIDER_APP_NAME = "fiveg_n4_provider_app"
+SDCORE_MANAGEMENT_RELATION_NAME = "sdcore-management"
+TEST_SDCORE_MANAGEMENT_PROVIDER_APP_NAME = "sdcore_management_provider_app"
 
 
 class TestCharm(unittest.TestCase):
@@ -23,55 +25,86 @@ class TestCharm(unittest.TestCase):
         self.addCleanup(self.harness.cleanup)
         self.harness.begin()
 
-    def test_given_cant_connect_to_container_when_config_changed_then_status_is_waiting(self):
-        self.harness.set_can_connect(container="nms", val=False)
-
-        self.harness.update_config(key_values={"webui-endpoint": "banana"})
-
+    def test_given_cant_connect_to_container_when_configure_sdcore_nms_then_status_is_waiting(
+        self,
+    ):
+        self.harness.charm._configure_sdcore_nms(event=Mock())
         self.assertEqual(
             self.harness.model.unit.status, WaitingStatus("Waiting for container to be ready")
         )
 
     def test_given_fiveg_n4_relation_not_created_when_pebble_ready_then_status_is_blocked(self):
-        self.harness.container_pebble_ready(container_name="nms")
-
+        self.harness.add_relation(
+            relation_name=SDCORE_MANAGEMENT_RELATION_NAME,
+            remote_app=TEST_SDCORE_MANAGEMENT_PROVIDER_APP_NAME,
+        )
+        self.harness.container_pebble_ready("nms")
         self.assertEqual(
             self.harness.model.unit.status,
             BlockedStatus(f"Waiting for `{FIVEG_N4_RELATION_NAME}` relation to be created"),
         )
 
-    def test_given_fiveg_n4_relation_not_created_when_config_changed_then_status_is_blocked(self):
-        self.harness.set_can_connect(container="nms", val=True)
-
-        self.harness.update_config(key_values={"webui-endpoint": "banana"})
-
-        self.assertEqual(
-            self.harness.model.unit.status,
-            BlockedStatus(f"Waiting for `{FIVEG_N4_RELATION_NAME}` relation to be created"),
-        )
-
-    @patch("ops.model.Container.exists")
-    def test_given_config_not_valid_when_config_changed_then_status_is_waiting(self, patch_exists):
-        patch_exists.return_value = True
-        self.harness.set_can_connect(container="nms", val=True)
+    def test_given_sdcore_management_relation_not_created_when_pebble_ready_then_status_is_blocked(
+        self,
+    ):
         self.harness.add_relation(
             relation_name=FIVEG_N4_RELATION_NAME,
             remote_app=TEST_FIVEG_N4_PROVIDER_APP_NAME,
         )
-
-        self.harness.update_config(key_values={"webui-endpoint": ""})
-
+        self.harness.container_pebble_ready("nms")
         self.assertEqual(
             self.harness.model.unit.status,
-            BlockedStatus("Invalid `webui-endpoint` config value"),
+            BlockedStatus(
+                f"Waiting for `{SDCORE_MANAGEMENT_RELATION_NAME}` relation to be created",
+            ),
         )
 
-    def test_given_config_is_valid_and_fiveg_n4_relation_is_created_when_config_changed_then_pebble_plan_is_applied(  # noqa: E501
+    def test_given_management_url_not_available_when_pebble_ready_then_status_is_waiting(self):
+        fiveg_n4_relation_id = self.harness.add_relation(
+            relation_name=FIVEG_N4_RELATION_NAME,
+            remote_app=TEST_FIVEG_N4_PROVIDER_APP_NAME,
+        )
+        self.harness.update_relation_data(
+            relation_id=fiveg_n4_relation_id,
+            app_or_unit=TEST_FIVEG_N4_PROVIDER_APP_NAME,
+            key_values={"upf_hostname": "some.host.name", "upf_port": "1234"},
+        )
+        self.harness.add_relation(
+            relation_name=SDCORE_MANAGEMENT_RELATION_NAME,
+            remote_app=TEST_SDCORE_MANAGEMENT_PROVIDER_APP_NAME,
+        )
+        self.harness.container_pebble_ready("nms")
+        self.assertEqual(
+            self.harness.model.unit.status,
+            WaitingStatus("Waiting for webui management url to be available"),
+        )
+
+    def test_given_n4_information_not_available_when_pebble_ready_then_status_is_waiting(self):
+        self.harness.add_relation(
+            relation_name=FIVEG_N4_RELATION_NAME,
+            remote_app=TEST_FIVEG_N4_PROVIDER_APP_NAME,
+        )
+        sdcore_management_relation_id = self.harness.add_relation(
+            relation_name=SDCORE_MANAGEMENT_RELATION_NAME,
+            remote_app=TEST_SDCORE_MANAGEMENT_PROVIDER_APP_NAME,
+        )
+        self.harness.update_relation_data(
+            relation_id=sdcore_management_relation_id,
+            app_or_unit=TEST_SDCORE_MANAGEMENT_PROVIDER_APP_NAME,
+            key_values={"management_url": "http://10.0.0.1:5000"},
+        )
+        self.harness.container_pebble_ready("nms")
+        self.assertEqual(
+            self.harness.model.unit.status,
+            WaitingStatus("Waiting for UPF information to be available"),
+        )
+
+    def test_given_environment_information_available_and_required_relations_created_when_pebble_ready_then_pebble_plan_is_applied(  # noqa: E501
         self,
     ):
         test_upf_hostname = "some.host.name"
         test_upf_port = "1234"
-        self.harness.set_can_connect(container="nms", val=True)
+        test_management_url = "http://10.0.0.1:5000"
         fiveg_n4_relation_id = self.harness.add_relation(
             relation_name=FIVEG_N4_RELATION_NAME,
             remote_app=TEST_FIVEG_N4_PROVIDER_APP_NAME,
@@ -82,7 +115,15 @@ class TestCharm(unittest.TestCase):
             key_values={"upf_hostname": test_upf_hostname, "upf_port": test_upf_port},
         )
 
-        self.harness.update_config(key_values={"webui-endpoint": "banana"})
+        sdcore_management_relation_id = self.harness.add_relation(
+            relation_name=SDCORE_MANAGEMENT_RELATION_NAME,
+            remote_app=TEST_SDCORE_MANAGEMENT_PROVIDER_APP_NAME,
+        )
+        self.harness.update_relation_data(
+            relation_id=sdcore_management_relation_id,
+            app_or_unit=TEST_SDCORE_MANAGEMENT_PROVIDER_APP_NAME,
+            key_values={"management_url": test_management_url},
+        )
 
         expected_plan = {
             "services": {
@@ -93,16 +134,17 @@ class TestCharm(unittest.TestCase):
                     "environment": {
                         "UPF_HOSTNAME": test_upf_hostname,
                         "UPF_PORT": int(test_upf_port),
-                        "WEBUI_ENDPOINT": "banana",
+                        "WEBUI_ENDPOINT": test_management_url,
                     },
                 }
             }
         }
+        self.harness.container_pebble_ready("nms")
         updated_plan = self.harness.get_container_pebble_plan("nms").to_dict()
 
         self.assertEqual(expected_plan, updated_plan)
 
-    def test_given_config_is_valid_and_fiveg_n4_relation_is_created_when_config_changed_then_status_is_active(  # noqa: E501
+    def test_given_environment_information_available_and_required_relations_created_when_pebble_ready_then_status_is_active(  # noqa: E501
         self,
     ):
         self.harness.set_can_connect(container="nms", val=True)
@@ -116,6 +158,14 @@ class TestCharm(unittest.TestCase):
             key_values={"upf_hostname": "some.host.name", "upf_port": "1234"},
         )
 
-        self.harness.update_config({"webui-endpoint": "banana"})
+        sdcore_management_relation_id = self.harness.add_relation(
+            relation_name=SDCORE_MANAGEMENT_RELATION_NAME,
+            remote_app=TEST_SDCORE_MANAGEMENT_PROVIDER_APP_NAME,
+        )
+        self.harness.update_relation_data(
+            relation_id=sdcore_management_relation_id,
+            app_or_unit=TEST_SDCORE_MANAGEMENT_PROVIDER_APP_NAME,
+            key_values={"management_url": "http://10.0.0.1:5000"},
+        )
 
         self.assertEqual(self.harness.model.unit.status, ActiveStatus())
