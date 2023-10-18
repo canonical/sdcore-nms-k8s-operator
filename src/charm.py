8d931bb721550d1298d056c14f169507007ad15d
Patricia Reinoso <patricia.reinoso@canonical.com>
2023-10-18 16:02:53 +0000

    Improve function naming
    Signed-off-by: Patricia Reinoso <patricia.reinoso@canonical.com>

--- src/charm.py
+++ src/charm.py
@@ -20,7 +20,7 @@ from lightkube.models.core_v1 import ServicePort
 from ops.charm import CharmBase
 from ops.framework import EventBase
 from ops.main import main
-from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
+from ops.model import ActiveStatus, BlockedStatus, Relation, WaitingStatus
 from ops.pebble import Layer
 
 logger = logging.getLogger(__name__)
@@ -147,14 +147,14 @@ class SDCoreNMSOperatorCharm(CharmBase):
         """
         if gnb_identity_relations := self.model.relations.get(GNB_IDENTITY_RELATION_NAME):
             for relation in gnb_identity_relations:
-                if not self._get_gnb_name(relation) or not self._get_tac(relation):
+                if not self._get_gnb_name(relation) or not self._get_gnb_tac(relation):
                     logger.warning(
                         "Invalid information in %s integration with %s",
                         GNB_IDENTITY_RELATION_NAME,
                         relation.app,
                     )
 
-    def _get_gnb_name(self, gnb_identity_relation) -> str:
+    def _get_gnb_name(self, gnb_identity_relation: Relation) -> str:
         """Gets gNB name from the `fiveg_gnb_identity` relation data bag.
 
         Returns:
@@ -166,7 +166,7 @@ class SDCoreNMSOperatorCharm(CharmBase):
             )
         return gnb_identity_relation.data[gnb_identity_relation.app].get("gnb_name", "")
 
-    def _get_tac(self, gnb_identity_relation) -> Optional[int]:
+    def _get_gnb_tac(self, gnb_identity_relation: Relation) -> Optional[int]:
         """Gets TAC from the `fiveg_gnb_identity` relation data bag.
 
         Returns:
