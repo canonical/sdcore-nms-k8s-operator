c3f78eff13457ec0d7e0f6320b2e6e1fb2812ef8
Patricia Reinoso <patricia.reinoso@canonical.com>
2024-08-26 15:23:00 +0000

    fix charm conf

--- tests/interface/conftest.py
+++ tests/interface/conftest.py
@@ -12,36 +12,42 @@ from charm import SDCoreNMSOperatorCharm
 def interface_tester(interface_tester: InterfaceTester):
 
     with tempfile.TemporaryDirectory() as tempdir:
-
         auth_database_relation = scenario.Relation(
-                endpoint="auth_database",
-                interface="mongodb_client",
-                remote_app_data={
-                    "username": "apple",
-                    "password": "hamburger",
-                    "uris": "1.2.3.4:1234",
-                },
-            )
+            endpoint="auth_database",
+            interface="mongodb_client",
+            remote_app_data={
+                "username": "apple",
+                "password": "hamburger",
+                "uris": "1.2.3.4:1234",
+            },
+        )
         common_database_relation = scenario.Relation(
-                endpoint="common_database",
-                interface="mongodb_client",
-                remote_app_data={
-                    "username": "banana",
-                    "password": "pizza",
-                    "uris": "2.3.1.1:1234",
-                },
-            )
+            endpoint="common_database",
+            interface="mongodb_client",
+            remote_app_data={
+                "username": "banana",
+                "password": "pizza",
+                "uris": "1.1.1.1:1234",
+            },
+        )
         config_mount = scenario.Mount(
-                    location="/nms/config",
-                    src=tempdir,
-                )
+            location="/nms/config",
+            src=tempdir,
+        )
         container = scenario.Container(
-                    name="nms",
-                    can_connect=True,
-                    mounts={"config": config_mount},
-                    layers={"nms": Layer({"services": {"nms": {}}})},
-                    service_status={"nms": ServiceStatus.ACTIVE},
-                )
+            name="nms",
+            can_connect=True,
+            mounts={"config": config_mount},
+            layers={"nms": Layer({"services": {"nms": {}}})},
+            service_status={"nms": ServiceStatus.ACTIVE},
+        )
+
+        with open(f"{tempdir}/webuicfg.conf", "w") as f:
+            f.write("whatever config file content")
+
+        #state_out = self.ctx.run("collect_unit_status", state_in)
+
+        #assert state_out.unit_status == ActiveStatus()
 
         interface_tester.configure(
             charm_type=SDCoreNMSOperatorCharm,
