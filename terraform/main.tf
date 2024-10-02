7494391118dbd7288e7455744b955490ba5cc901
Ghislain Bourgeois <ghislain.bourgeois@canonical.com>
2024-10-02 11:20:45 -0400

    chore: Refactor the Terraform module to follow spec CC006

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,21 @@
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-resource "juju_application" "sdcore-nms-k8s" {
+resource "juju_application" "nms" {
   name  = var.app_name
-  model = var.model_name
+  model = var.model
 
   charm {
-    name    = "sdcore-nms-k8s"
-    channel = var.channel
+    name     = "sdcore-nms-k8s"
+    channel  = var.channel
+    revision = var.revision
   }
-  units = 1
-  trust = true
+
+  config      = var.config
+  constraints = var.constraints
+  units       = var.units
+  resources   = var.resources
+  trust       = true
 }
 
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,6 @@
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-variable "model_name" {
-  description = "Name of Juju model to deploy application to."
-  type        = string
-  default     = ""
-}
-
 variable "app_name" {
   description = "Name of the application in the Juju model"
   type        = string
@@ -17,4 +11,46 @@ variable "channel" {
   description = "The channel to use when deploying a charm."
   type        = string
   default     = "1.5/edge"
+}
+
+variable "config" {
+  description = "Application config. Details about available options can be found at https://charmhub.io/sdcore-nms-k8s-operator/configure."
+  type        = map(string)
+  default     = {}
+}
+
+variable "constraints" {
+  description = "Juju constraints to apply for this application."
+  type        = string
+  default     = "arch=amd64"
+}
+
+variable "model" {
+  description = "Reference to a `juju_model`."
+  type        = string
+  default     = ""
+}
+
+variable "resources" {
+  description = "Resources to use with the application. Details about available options can be found at https://charmhub.io/sdcore-nms-k8s-operator/configure."
+  type        = map(string)
+  default     = {}
+}
+
+variable "revision" {
+  description = "Revision number of the charm"
+  type        = number
+  default     = null
+}
+
+variable "units" {
+  description = "Number of units to deploy"
+  type        = number
+  default     = 1
+
+  validation {
+    condition     = var.units == 1
+    error_message = "Scaling is not supported for this charm."
+  }
+
 }
